32e9f45b4a714241658dad7940a3dd94e7245099
Marcus Price <marcusprice88@gmail.com>
2019-05-02 10:25:24 -0700

    Assignment 5 Resource View

--- techreviewproj/techapp/urls.py
+++ techreviewproj/techapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 #this is a comment
 urlpatterns = [
     path('', views.index, name='index'),
+    path('gettypes/', views.gettypes, name='types'),
 ]
--- techreviewproj/techapp/views.py
+++ techreviewproj/techapp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
+from .models import TechType, Product, Review
 
 # Create your views here.
 def index(request):
     return render(request, 'techapp/index.html')
-    
+
+def gettypes(request):
+    type_list=TechType.objects.all()
+    return render(request, 'techapp/types.html', {'type_list' : type_list})
