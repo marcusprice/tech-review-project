c835565503e758b86973e0e3a79f7d04a6d46d64
Marcus Price <marcusprice88@gmail.com>
2019-05-07 09:44:23 -0700

    Assignment 6 Meeting and detail

--- techreviewproj/techapp/urls.py
+++ techreviewproj/techapp/urls.py
@@ -5,4 +5,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('gettypes/', views.gettypes, name='types'),
+    path('getproducts/', views.getproducts, name='products'),
+    path('productdetails/<int:id>', views.productdetails, name='productdetails'),    
 ]
--- techreviewproj/techapp/views.py
+++ techreviewproj/techapp/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import TechType, Product, Review
 
 # Create your views here.
@@ -8,3 +8,16 @@ def index(request):
 def gettypes(request):
     type_list=TechType.objects.all()
     return render(request, 'techapp/types.html', {'type_list' : type_list})
+
+def getproducts(request):
+    products_list=Product.objects.all()
+    return render(request, 'techapp/products.html', {'products_list': products_list})
+
+def productdetails(request, id):
+    prod=get_object_or_404(Product, pk=id)
+    reviews=Review.objects.filter(product=id).count()
+    context={
+        'prod' : prod,
+        'reviews' : reviews,
+    }
+    return render(request, 'techapp/productdetails.html', context=context)
